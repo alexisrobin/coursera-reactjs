c72a4efd671bb49a7ad828b4a5eeaaef99e40a4b
alexisrobin <alexisrobin.bonjour@gmail.com>
2018-10-06 10:58:44 +0200

    Controlled forms

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 
-import BrowserReactRouter, { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
